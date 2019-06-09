1f5c184816eb5455bc4cdded1932b247b49fbb30
Young-Ki Kim <taogone@gmail.com>
2019-06-09 14:47:17 +0900

    20190609_14:47

--- 3rdWeek/exp05_login.js
+++ 3rdWeek/exp05_login.js
@@ -4,9 +4,30 @@ var mongoClient = require("mongodb").MongoClient;
 var router = express.Router();
 var app = express();
 var path = require("path");
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var expressSession = require("express-session");
+var cookieParser = require("cookie-parser");
+var static = require("serve-static");
 
+// View Tamplate 등록
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Router 등록
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  expressSession({
+    secret: "my key",
+    resave: true,
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 // 몽고디비 연결 설정
 var db;
@@ -18,42 +39,67 @@ function connectDB() {
   ) {
     if (err) throw err;
     console.log("MogoDB is connected: " + dbUrl);
-    db = database.db("vehicle");
+    console.log(path.join(__dirname, "public"));
+    db = database.db("local");
   });
 }
 
-function carlist(db, callback) {
-  // MongoDB 연결 등 처리
-  var car = db.collection("car");
-  car.find({}).toArray(function(err, docs) {
+function authUser(db, loginData, callback) {
+  // 사용자 인증 확인 함수
+  var users = db.collection("users");
+  users.find(loginData).toArray(function(err, docs) {
     if (err) {
       callback(err, null);
+      return;
     }
+
     if (docs.length > 0) {
+      console.log("사용자 있음");
       callback(null, docs);
     } else {
-      callback(null, docs);
+      console.log("사용자 없음");
+      callback(null, null);
     }
   });
 }
 
 // Routing Configuration
-router.route("/car_list").get(function(req, res) {
-  console.log("/car_list 요청됨.");
-  if (db) {
-    // 콜백 함수를 활용한 처리
-    carlist(db, function(err, result) {
-      // View Page
-      if (err) throw err;
-      console.log(result);
+router.route("/process/login").post(function(req, res) {
+  //요청 파라미터 확인
+  var userId = req.body.id;
+  var passwd = req.body.passwd;
+  var loginData = {
+    id: userId,
+    password: passwd
+  };
+  console.dir(loginData);
 
-      req.app.render("car_list", { carList: result }, function(err, html) {
-        res.end(html);
-      });
+  if (db) {
+    authUser(db, loginData, function(err, docs) {
+      if (err) {
+        throw err;
+      }
+      if (docs) {
+        req.session.user = {
+          id: docs[0].id,
+          name: docs[0].name
+        };
+        res.writeHead(200, { "Content-Type": "text/html;charset=utf8" });
+        res.write(`<h3>로그인 성공</h3>`);
+        res.write(`<p>아이디:${docs[0].id}</p>`);
+        res.write(`<p>이름:${docs[0].name}</p>`);
+        res.end();
+      } else {
+        res.end("no user");
+      }
     });
   } else {
-    res.end("MongoDB connection error");
+    res.end("no db connection");
   }
+});
+
+router.route("/process/logout").get(function(req, res) {
+  console.log("/process/login 요청됨.");
 });
 
 app.use("/", router);
